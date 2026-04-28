97d544794229dedab80fc0dd8eff803c75c81e79
Emerson Santana <emersonsouzasantana@outlook.com>
2026-04-28 11:22:13 -0300

    Project: Contract Processing System - Services and Interfaces

---
+++ 02_ContractProcessing_Interface/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpStudies.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
