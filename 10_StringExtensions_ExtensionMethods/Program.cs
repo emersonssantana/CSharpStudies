873c3fcff76d2431feab6e171da0b85e0483b13d
Emerson Santana <emersonsouzasantana@outlook.com>
2026-05-18 12:51:10 -0300

    10 - String Extensions: Extension methods and custom string manipulation studies

---
+++ 10_StringExtensions_ExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpStudies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
+
+
+    
