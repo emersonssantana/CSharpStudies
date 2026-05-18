f117383176d5f64f3b0ac2f9cf07d7072f0d8dd9
Emerson Santana <emersonsouzasantana@outlook.com>
2026-05-18 12:20:05 -0300

    09 - DateTime Extensions: Extension methods and elapsed time processing studies

---
+++ CSharpStudies/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpStudies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2026, 5, 18, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+        }
+    }
+}
+
+
+    
